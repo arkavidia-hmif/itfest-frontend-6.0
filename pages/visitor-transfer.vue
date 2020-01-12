0439d4f10513502d1d43fe25e33d106d95fb01c0
mkamadeus <mkamadeus.mka@gmail.com>
2020-01-12 23:40:15 +0700

    Make layout more similar in point transfer interface

--- pages/visitor-transfer.vue
+++ pages/visitor-transfer.vue
@@ -1,41 +1,37 @@
 <template>
   <v-row align="center" justify="center">
     <v-col class="pa-5" cols="10">
-      <v-card>
-        <v-list>
-          <v-list-item>
-            <v-form>
-              <div class="d-flex align-center">
-                <div class="px-2 transfer-input">
-                  <v-text-field label="You're giving.." color="red" />
-                </div>
-                <div class="px-2">
-                  points
-                </div>
-              </div>
-              <div class="my-4">
-                <div class="form-label">
-                  Account destination
-                </div>
-                <div class="account-number">
-                  882947
-                </div>
-              </div>
-              <div class="my-4">
-                <div class="form-label">
-                  Points left after giving
-                </div>
-                <div class="points-balance">
-                  350
-                </div>
-              </div>
-            </v-form>
-          </v-list-item>
-        </v-list>
-      </v-card>
+      <v-form>
+        <div class="my-4">
+          <div class="form-label">
+            You're giving..
+          </div>
+          <div class="d-flex align-center">
+            <div class="px-2 transfer-input">
+              <v-text-field label="You're giving..." full-width suffix="points" />
+            </div>
+          </div>
+        </div>
+        <div class="my-4">
+          <div class="form-label">
+            Account destination
+          </div>
+          <div class="account-number">
+            882947
+          </div>
+        </div>
+        <div class="my-4">
+          <div class="form-label">
+            Points left after giving
+          </div>
+          <div class="points-balance">
+            350
+          </div>
+        </div>
+      </v-form>
     </v-col>
-    <v-col class="d-flex justify-center">
-      <v-btn color="rgb(22, 26, 255)">
+    <v-col class="d-flex justify-center" cols="10">
+      <v-btn color="#4336D7" class="white--text" height="50px" width="100%">
         Give Points!
       </v-btn>
     </v-col>
@@ -46,12 +42,20 @@
 .account-number {
   font-size:2em;
   font-weight:600;
-  color:rgb(255, 22, 53);
+  color:#FF0B51;
 }
 .points-balance {
   font-size:4em;
   font-weight:600;
   color:rgb(22, 26, 255);
+}
+</style>
+
+<style scoped>
+::v-deep .v-text-field__slot > input{
+  color:#FF0B51;
+  font-size:1.2em;
+  font-weight:600;
 }
 </style>
 
