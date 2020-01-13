463d2ee3620968a0f11ee47d8df8e9dd8346184b
mkamadeus <mkamadeus.mka@gmail.com>
2020-01-13 12:05:45 +0700

    Temporary fix to layout

--- layouts/submenu.vue
+++ layouts/submenu.vue
@@ -1,39 +1,10 @@
 <template>
   <v-app>
-    <v-container class="pa-0" fluid>
-      <v-row>
-        <v-col class="back-header pa-0" cols="12">
-          <v-toolbar height="86" class="px-4">
-            <v-btn icon>
-              <v-icon>mdi-arrow-left</v-icon>
-            </v-btn>
-            <v-toolbar-title>
-              Title
-            </v-toolbar-title>
-          </v-toolbar>
-        </v-col>
-        <v-col cols="12">
-          <nuxt />
-        </v-col>
-      </v-row>
+    <v-container class="pa-0" fluid fill-height>
+      <nuxt />
     </v-container>
   </v-app>
 </template>
-
-<style lang="scss">
-* {
-  font-family: 'Raleway', sans-serif;
-  color: #000;
-}
-</style>
-
-<style lang="scss" scoped>
-.back-header{
-  background-color:white;
-  font-weight: 600;
-  font-size: 1.2em;
-}
-</style>
 
 <script>
 export default {}
--- pages/visitor-transfer.vue
+++ pages/visitor-transfer.vue
@@ -1,44 +1,59 @@
 <template>
-  <v-row align="center" justify="center">
-    <v-col class="pa-5" cols="10">
-      <v-form>
-        <div class="my-4">
-          <div class="form-label">
-            You're giving..
-          </div>
-          <div class="d-flex align-center">
-            <div class="px-2 transfer-input">
-              <v-text-field label="You're giving..." full-width suffix="points" />
-            </div>
-          </div>
-        </div>
-        <div class="my-4">
-          <div class="form-label">
-            Account destination
-          </div>
-          <div class="account-number">
-            882947
-          </div>
-        </div>
-        <div class="my-4">
-          <div class="form-label">
-            Points left after giving
-          </div>
-          <div class="points-balance">
-            350
-          </div>
-        </div>
-      </v-form>
+  <v-row>
+    <v-col cols="12">
+      <v-toolbar height="86" class="px-4">
+        <v-btn icon>
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+          Title
+        </v-toolbar-title>
+      </v-toolbar>
     </v-col>
-    <v-col class="d-flex justify-center" cols="10">
-      <v-btn color="#4336D7" class="white--text text-none" height="50px" width="100%">
-        Give Points!
-      </v-btn>
+    <v-col cols="12">
+      <v-row align="center" justify="center">
+        <v-col class="pa-5" cols="10">
+          <v-form>
+            <div class="my-4">
+              <div class="form-label">
+                You're giving..
+              </div>
+              <div class="d-flex align-center">
+                <div class="px-2 transfer-input">
+                  <v-text-field full-width suffix="points" />
+                </div>
+              </div>
+            </div>
+            <div class="my-4">
+              <div class="form-label">
+                Account destination
+              </div>
+              <div class="account-number">
+                882947
+              </div>
+            </div>
+            <div class="my-4">
+              <div class="form-label">
+                Points left after giving
+              </div>
+              <div class="points-balance">
+                350
+              </div>
+            </div>
+          </v-form>
+        </v-col>
+        <v-col class="d-flex justify-center" cols="10">
+          <v-btn color="#4336D7" class="white--text text-none" height="50px" width="100%">
+            Give Points!
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
 
 <style lang="scss" scoped>
+
 .account-number {
   font-size:2em;
   font-weight:600;
