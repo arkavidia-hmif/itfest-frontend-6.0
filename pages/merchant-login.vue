ef1dad90e9a5e51a9ea5ed0b61cd01f7740ae957
mkamadeus <mkamadeus.mka@gmail.com>
2020-01-23 14:13:53 +0700

    Fixed merchant login

--- pages/merchant-login.vue
+++ pages/merchant-login.vue
@@ -1,7 +1,27 @@
 <template>
   <v-content class="merchant-login-container">
-    <v-container class="fill-height" fluid pa-12>
-      <MerchantLogin />
+    <v-container class="fill-height" fluid pa-6>
+      <v-row justify="center" justify-md="start" align="center" no-gutters>
+        <v-col
+          cols="12"
+          md="6"
+          lg="4"
+          align-center
+          justify-center
+        >
+          <MerchantLogin />
+        </v-col>
+        <v-col v-show="$vuetify.breakpoint.lgAndUp" lg="1" />
+        <v-col
+          cols="12"
+          md="6"
+          lg="5"
+          align-center
+          justify-center
+        >
+          <MerchantLoginForm :button-text="'Login'" />
+        </v-col>
+      </v-row>
     </v-container>
   </v-content>
 </template>
@@ -15,10 +35,12 @@
 <script lang="ts">
 import Vue from 'vue';
 import MerchantLogin from '~/components/merchant-login/MerchantLogin.vue';
+import MerchantLoginForm from '~/components/merchant-login/MerchantLoginForm.vue';
 
 export default Vue.extend({
   components: {
-    MerchantLogin
+    MerchantLogin,
+    MerchantLoginForm
   }
 });
 </script>
