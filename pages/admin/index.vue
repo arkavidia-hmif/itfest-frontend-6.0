3f48c2f00bec66baf08aea34b40bad78582540d7
Nella <nellazabrinapramata@yahoo.co.id>
2020-01-28 00:58:38 +0700

    Add Admin home page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <v-container />
+  <div>
+    <div class="ma-4 mt-10">
+      Signed in as <span class="ml-1" style="color: #FF0B51; font-size: 1.2rem; vertical-align: sub">Administrator</span>
+    </div>
+    <div>
+      <HomeButton label="Redeem Points" icon="fa-sync-alt" href="redeem-points" />
+      <HomeButton label="See Merch Stock" icon="fa-database" href="merchandise-stock" />
+      <HomeButton label="Update Merch Stock" icon="fa-cart-plus" href="update-stock" />
+    </div>
+  </div>
 </template>
 
+<style lang="scss" scoped>
+  * {
+    font-family: 'Raleway', sans-serif;
+    font-weight: 800;
+  }
+</style>
+
 <script>
-export default {};
+import HomeButton from '../../components/admin-stock/HomeButton';
+export default {
+  components: { HomeButton }
+};
 </script>
