217cc2853efaab2b601a4daa936fdfdf3f3a9ea9
mkamadeus <mkamadeus.mka@gmail.com>
2020-01-28 18:37:57 +0700

    added login visitor

---
+++ components/visitor-login/VisitorLoginForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-form v-model="isValid">
+    <v-text-field
+      v-model="emailAddress"
+      :rules="emailRules"
+      label="Email"
+    />
+    <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      label="Password"
+      type="password"
+    />
+      <v-btn
+        color="#FF084F"
+        class="white--text my-12"
+        x-large
+        block
+      >
+        Login!
+      </v-btn>
+    </div>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  components: { }
+})
+class VisitorRegisterForm extends Vue {
+  isValid: boolean = false;
+  emailAddress: string = '';
+  password: string = '';
+  emailRules = [
+    v => !!v || 'Email is required!',
+    v => /.+@.+/.test(v) || 'Must be a valid email address.'
+  ];
+  passwordRules = [
+    v => !!v || 'Password is required',
+  ];
+
+  get passwordsFilled(): boolean {
+    return (this.password !== '' && this.rePassword !== '');
+  }
+}
+
+export default VisitorRegisterForm;
+
+</script>
