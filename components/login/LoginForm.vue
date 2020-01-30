56c87f0aee7bc8d9e2a78b48abe70d52c53ecd55
Nella <nellazabrinapramata@yahoo.co.id>
2020-01-31 00:35:28 +0700

    fixing label input field

--- components/login/LoginForm.vue
+++ components/login/LoginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-form class="px-4 justify-center" @submit.prevent="attemptLogin">
-    <v-text-field v-model="username" label="Email Address" type="text" />
+    <v-text-field v-model="username" label="Username" type="text" />
     <v-text-field v-model="password" label="Password" type="password" />
     <div class="text-center">
       <v-btn :loading="isLoggingIn" type="submit" color="success" class="mt-4 px-12 py-2 text-none" rounded>
