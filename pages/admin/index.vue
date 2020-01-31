e1994d62d08ba47db83cb34d56e6b4210179006b
Nella <nellazabrinapramata@yahoo.co.id>
2020-01-31 18:12:25 +0700

    Update admin home buttons

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -4,10 +4,11 @@
       Signed in as <span class="ml-1" style="color: #FF0B51; font-size: 1.2rem; vertical-align: sub">Administrator</span>
     </div>
     <div>
-      <HomeButton label="Redeem Points" icon="fa-sync-alt" href="admin/redeem-points" />
+      <HomeButton label="Redeem Visitor Points" icon="fa-sync-alt" href="admin/scan-user" />
       <HomeButton label="See Merch Stock" icon="fa-database" href="admin/merchandise-stock" />
-      <HomeButton label="Update Merch Stock" icon="fa-cart-plus" href="admin/update-stock" />
-      <HomeButton label="Scan User" icon="fa-qrcode" href="admin/scan-user" />
+      <HomeButton label="Add Merch Stock" icon="fa-cart-plus" href="admin/add-stock" />
+      <HomeButton label="Update Merch Stock" icon="fa-edit" href="admin/update-stock" />
+      <HomeButton label="See Redemption History" icon="fa-history" href="admin/redemption-history" />
     </div>
   </div>
 </template>
