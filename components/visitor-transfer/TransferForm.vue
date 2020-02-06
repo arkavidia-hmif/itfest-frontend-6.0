c5233bd5fd52a75b7c6bc2e70672805228cc5a94
didithilmy <didithilmy@gmail.com>
2020-02-06 13:27:55 +0700

    Add visitor point transfer

--- components/visitor-transfer/TransferForm.vue
+++ components/visitor-transfer/TransferForm.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="pa-4">
-    <v-form>
-      <div class="my-4">
+    <v-form v-if="isUserLoaded">
+      <div v-if="!!errorMessage">
+        <v-alert
+          type="error"
+        >
+          {{ errorMessage }}
+        </v-alert>
+      </div>
+      <div>
         <div class="form-label">
           You're giving..
         </div>
-        <v-text-field full-width suffix="points" outlined class="mt-4" />
+        <v-text-field v-model="pointTemp" full-width suffix="points" outlined class="mt-4" />
       </div>
-      <div class="my-4">
-        <div class="form-label">
-          Account destination
-        </div>
-        <div class="account-number">
-          887964
-        </div>
+      <div class="form-label">
+        Recipient: <b style="color: #FF0B51">{{ qrName }}</b>
       </div>
-      <div class="my-4">
-        <div class="form-label">
-          Points left after giving
-        </div>
-        <div class="points-balance">
-          350
-        </div>
+      <div class="form-label mb-4">
+        Points left after giving: <b style="color: #3F32D5">{{ user.point - pointTemp }}</b>
       </div>
     </v-form>
-    <v-btn color="#4336D7" class="white--text text-none" block @click="process">
+    <v-btn
+      color="#4336D7"
+      class="white--text text-none"
+      block
+      :disabled="!valid"
+      :loading="isLoading"
+      @click="process"
+    >
       Give Points!
     </v-btn>
   </div>
@@ -48,13 +52,55 @@
 </style>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator';
-
+import { Vue, Component, Prop, Action, Getter } from 'nuxt-property-decorator';
+import { UserData } from "~/api/types";
 
 @Component({})
 class TransferPoints extends Vue {
+  @Prop() qrid!: string;
+  @Action('user/fetchUser') fetchUserAction;
+  @Getter('user/getUser') user!: UserData;
+  @Action('game/playGame') playAction;
+  @Action('game/getQrDetails') getQrDetailsAction;
+  @Action('user/transferPoint') transferPointAction;
+
+  selected: Array<string> = [];
+  pointTemp: number = 0;
+  isUserLoaded: boolean = false;
+  errorMessage: string = '';
+  isLoading: boolean = true;
+  qrName: string = "";
+
+  mounted() {
+    this.isLoading = true;
+    this.getQrDetailsAction({ qrId: this.qrid })
+      .then(qr => {
+        this.qrName = qr.name;
+        this.isLoading = false;
+      });
+    this.fetchUserAction().finally(()=>{
+      this.isUserLoaded = true;
+    });
+  }
+
+  get valid() {
+    const { point } = this.user;
+      return this.pointTemp <= (point || 0);
+  }
+
   process() {
-    this.$emit('close');
+    if (this.valid) {
+      this.transferPointAction({recipientQrId: this.qrid, amount: this.pointTemp})
+        .then(() => {
+          this.$emit('close');
+        })
+        .catch(() => {
+          // TODO show error
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    }
   }
 }
 
